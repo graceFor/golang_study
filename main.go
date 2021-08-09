07ef2ec93c607c8c5c5ef8a59023183a1faf6279
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-08-09 15:07:44 +0900

    function(defer, naked

--- main.go
+++ main.go
@@ -5,37 +5,20 @@ import (
 	"strings"
 )
 
-func lenAndupper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
-}
-
-// func multiply(a int, b int) int {
-// 	return a * b
+// func lenAndupper(name string) (int, string) {
+// 	return len(name), strings.ToUpper(name)
 // }
-func multiply(a, b int) int {
-	return a * b
-}
 
-// ...은 내가 원하는 만큼 파라미터를 전달하는 방법
-func repeatMe(words ...string) {
-	fmt.Println((words))
+func lenAndupper(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done")
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
 }
 
 func main() {
-	repeatMe("hyun", "kyung", "kim", "g")
 
-	// 상수  = 값 변경 X
-	const name string = "hyun"
-	fmt.Println(name)
-
-	// 변수  = 값 변경 O
-	//var name1 string = "kyung"
-	name1 := "kyung"
-	name1 = "kyungs"
-	fmt.Println((name1))
-
-	fmt.Println(multiply(3, 4))
-	totalLength, _ := lenAndupper("hyunkyung")
-	fmt.Println(totalLength)
+	totalLength, upper := lenAndupper("hyunkyung")
+	fmt.Println(totalLength, upper)
 
 }
