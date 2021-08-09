910d9d310de025da59d18049874e6c4174debac4
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-08-09 13:56:42 +0900

    function

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func lenAndupper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
+}
+
+// func multiply(a int, b int) int {
+// 	return a * b
+// }
+func multiply(a, b int) int {
+	return a * b
+}
+
+// ...은 내가 원하는 만큼 파라미터를 전달하는 방법
+func repeatMe(words ...string) {
+	fmt.Println((words))
+}
 
 func main() {
-	fmt.Println("Hello world")
+	repeatMe("hyun", "kyung", "kim", "g")
+
+	// 상수  = 값 변경 X
+	const name string = "hyun"
+	fmt.Println(name)
+
+	// 변수  = 값 변경 O
+	//var name1 string = "kyung"
+	name1 := "kyung"
+	name1 = "kyungs"
+	fmt.Println((name1))
+
+	fmt.Println(multiply(3, 4))
+	totalLength, _ := lenAndupper("hyunkyung")
+	fmt.Println(totalLength)
+
 }
---
+++ something/something.go
@@ -0,0 +1,12 @@
+package something
+
+import "fmt"
+
+func sayBye() {
+	fmt.Println("Bye")
+}
+
+// 대문자로 시작함
+func SayHello() {
+	fmt.Println("Hello")
+}
