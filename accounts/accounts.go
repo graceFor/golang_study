d94ee648dd9a194edc0274cce4102e62ce6a1184
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-08-11 00:47:58 +0900

    account

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -11,3 +11,18 @@ func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
+
+// Despite x amout on your account
+func (a *Account) Despite(amount int) {
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+//Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) {
+	a.balance -= amount
+}
--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 
 func main() {
 	account := accounts.NewAccount("hyun")
-	fmt.Println(account)  // &{hyun 0}
-	fmt.Println(*account) // {hyun 0}
+	//fmt.Println(account)  // &{hyun 0}
+	//fmt.Println(*account) // {hyun 0}
+	account.Despite(10)
+	fmt.Println(account.Balance())
+	account.Withdraw(10)
+	fmt.Println(account.Balance())
 }
