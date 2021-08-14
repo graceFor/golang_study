8300ba2562b831af8bfa2628addb4ecf7f4b7e34
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-08-14 22:05:45 +0900

    Dictionary_update,delete

--- main.go
+++ main.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/graceFor/learngo/mydict"
 )
 
 func main() {
 	dictionary := mydict.Dictionary{"first": "First word"}
 	word := "hello"
-	definition := "Greeating"
-	err := dictionary.Add(word, definition)
-	if err != nil {
-		fmt.Println(err)
-	}
-	searchDefinition, _ := dictionary.Search(word)
-	fmt.Println("found:", word, ",definition:", searchDefinition)
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
-	}
+	dictionary.Add(word, "First")
+	dictionary.Update(word, "Second")
 
 }
