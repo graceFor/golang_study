825c37bc18a04c0872d4dddb459fa48e05be8cda
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-08-20 23:26:52 +0900

    clear

--- main.go
+++ main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/graceFor/learngo/mydict"
-)
-
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	baseword := "hello"
-	dictionary.Add(baseword, "First")
-	word, _ := dictionary.Search(baseword)
-	fmt.Println(word)
-	dictionary.Delete(baseword)
-	word, err := dictionary.Search(baseword)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(word)
+
 }
