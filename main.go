fcba509983f1fd0578f2b3b244d67ece054026d2
60162260_KimHyunKyung <tbgusrud12@gmail.com>
2021-08-23 14:05:57 +0900

    channel

--- main.go
+++ main.go
@@ -1,47 +1,24 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("Request failed")
-
 func main() {
-	var results = make(map[string]string)
-
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
+	c := make(chan string)
+	people := [5]string{"kim", "hyun", "a", "b", "c"}
+	for _, person := range people {
+		go isSexy(person, c)
 	}
-
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
+	fmt.Println("Waiting for messages")
+	for i := 0; i < len(people); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking: ", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
-	}
-	return nil
+// "kim", "hyun"이 섹시한지를 체크
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 10)
+	c <- person + " is sexy"
 }
